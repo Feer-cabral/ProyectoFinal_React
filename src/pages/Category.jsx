1c3563ae5b016d0050b78ad1faa3d1e27eb99aee
Feer-cabral <cabralfernando782@gmail.com>
2023-12-05 01:55:13 -0300

    creo una query para filtrar productos por categoria

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,12 +1,41 @@
 import React from "react";
 
 import { ItemListContainer, LoaderComponent } from "../components";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 
 export const Category = () => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
   const { categoryId } = useParams();
 
-  const { products, loading, error } = useProductsByCategory(categoryId);
+  // const { products, loading, error } = useProductsByCategory(categoryId);
+
+  useEffect(() => {
+    const db = getFirestore();
+
+    const categoryQuery = query(
+      collection(db, "products"),
+      where("category", "==", categoryId)
+    );
+
+    getDocs(categoryQuery)
+      .then((snapshot) => {
+        setProducts(
+          snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        );
+      })
+      .catch((err) => console.err(err))
+      .finally(() => setLoading(false));
+  }, [categoryId]);
 
   return loading ? (
     <LoaderComponent />
